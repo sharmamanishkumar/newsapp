a1527083d3009c496b3954bcae1cebff26c72e8e
sharmamanishkumar <13bcah19@gmail.com>
2020-10-06 17:23:20 -0700

    add OPTIONS in cors error

--- app.js
+++ app.js
@@ -3,59 +3,67 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const userRoutes = require("./api/routers/users");
-const dashboardRouter = require("./api/routers/dashboard")
+const dashboardRouter = require("./api/routers/dashboard");
 const app = express();
 
 mongoose.connect(
-    process.env.MOGNO_CLUSTER_APP ||
+  process.env.MOGNO_CLUSTER_APP ||
     "mongodb+srv://manish:" +
-    process.env.MONGO_ATLAS_PW +
-    "@cluster0.lvk1u.mongodb.net/" + process.env.DB + "?retryWrites=true&w=majority", {
+      process.env.MONGO_ATLAS_PW +
+      "@cluster0.lvk1u.mongodb.net/" +
+      process.env.DB +
+      "?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-}
+  }
 );
 mongoose.Promise = global.Promise;
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
     res.header(
-        "Access-Control-Allow-Header",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+      "Access-Control-Allow-Methods",
+      "PUT,POST,DELETE,PATCH,GET,OPTIONS"
     );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE,PATCH,GET");
-        return res.status(200).json({});
-    }
-    next();
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.use("/news", userRoutes)
-app.use("/dashboards", dashboardRouter)
-app.use('/', (req, res) => {
-    res.send('Welcome sir ')
-})
+app.use("/news", userRoutes);
+app.use("/dashboards", dashboardRouter);
+app.use("/", (req, res) => {
+  res.send("Welcome sir ");
+});
 app.use((req, res, next) => {
-    const error = new Error("i am not found");
-    error.status = 404;
-    next(error);
+  const error = new Error("i am not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message,
-        },
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
