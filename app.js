a229150176f766ffbb0120ca686e4563a3025422
sharmamanishkumar <13bcah19@gmail.com>
2020-10-07 22:00:17 -0700

    remove cors error to add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const userRoutes = require("./api/routers/users");
 const dashboardRouter = require("./api/routers/dashboard");
+const cors = require("cors");
 const app = express();
 
 mongoose.connect(
@@ -29,22 +30,22 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Header",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "PUT,POST,DELETE,PATCH,GET,OPTIONS"
-    );
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Header",
+//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "PUT,POST,DELETE,PATCH,GET,OPTIONS"
+//     );
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 app.use("/news", userRoutes);
 app.use("/dashboards", dashboardRouter);
